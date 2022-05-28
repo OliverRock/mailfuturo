1a8905694bd4436fe590de25f96325f9d4d71ef2
OliverRockPayFit <51485881+OliverRockPayFit@users.noreply.github.com>
2022-05-28 15:19:34 +0200

    Fix HTML spacing

--- src/public/js/homePage.js
+++ src/public/js/homePage.js
@@ -3,11 +3,13 @@ let timePeriod = document.getElementById('timePeriodDropDown');
 
 updateDeliveryDate();
 
-deliveryDate.addEventListener('change', () => {
+timePeriod.addEventListener('change', () => {
+  console.log('hell');
   updateDeliveryDate();
 });
 
 function updateDeliveryDate() {
+  console.log('hellp');
   var today = new Date();
   var day = today.getDate();
   var mon = today.getMonth() + 1; //January is 0!
@@ -16,9 +18,8 @@ function updateDeliveryDate() {
   let mm = String(mon).padStart(2, '0');
   let yyyy = today.getFullYear();
 
-  if (timePeriod != 99) {
+  if (timePeriod.value != 99) {
     yyyy = today.getFullYear() + parseInt(timePeriod.value);
     deliveryDate.value = yyyy + '-' + mm + '-' + dd;
   }
-  console.log('hell');
 }
